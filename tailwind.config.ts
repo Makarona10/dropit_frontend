59b2f5f5a9408898381d8fc0c61f9736c4cf13fc
Makarona10 <ahmedneyssi@gmail.com>
2025-02-04 22:03:49 +0200

    Update -> Packages and configurations updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
+          primary: {
+            100: "#F50B2E",
+            200: "#E91938",
+            300: "#E2223F",
+            400: "#D92A44",
+            500: "#c83c51",
+            600: "#A53042",
+            700: "#942938",
+            800: "#862231",
+            900: "#721D2A",
+            950: "#601621",
+          },
       },
     },
   },
