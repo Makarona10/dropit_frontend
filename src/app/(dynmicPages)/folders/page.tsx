18f469039ef598083a57cd16b44eadc08e6290d0
Makarona10 <ahmedneyssi@gmail.com>
2025-04-21 02:36:32 +0200

    Build -> Folders page constructed

---
+++ src/app/(dynmicPages)/folders/page.tsx
@@ -0,0 +1,11 @@
+import SideBar from "@/app/components/common/SideBar";
+
+const FoldersPage = () => {
+  return (
+    <div className="w-full flex">
+      <SideBar />
+    </div>
+  );
+};
+
+export default FoldersPage;
