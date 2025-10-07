371a9dd9550dd75680a70dc61e9f84e73862e4de
Makarona10 <ahmedneyssi@gmail.com>
2025-10-07 15:51:45 +0300

    Enhance -> Created a Modal component to build all app modals with

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children?: ReactNode;
+}
+
+export default function Modal({ isOpen, onClose, children }: ModalProps) {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="cursor-default fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          onClick={(e) => {
+            e.stopPropagation();
+            onClose();
+          }}
+        >
+          <motion.div
+            className="bg-neutral-900 rounded-xl border-2 border-neutral-600 p-4"
+            initial={{ scale: 0.9, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.9, opacity: 0 }}
+            transition={{ type: "spring", damping: 40, stiffness: 900 }}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
