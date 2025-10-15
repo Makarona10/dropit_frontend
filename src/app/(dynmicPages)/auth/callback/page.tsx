e3c718b9bf379f5de346b29682a89f38a0d8c2a3
Makarona10 <ahmedneyssi@gmail.com>
2025-10-15 17:20:52 +0300

    Fix -> App is ready for building

--- src/app/(dynmicPages)/auth/callback/page.tsx
+++ src/app/(dynmicPages)/auth/callback/page.tsx
@@ -1,27 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import LoadingSpinner from "@/components/common/LoadingSpinner";
+import GoogleCallback from "@/components/pages/auth/callback/GoogleCallBack";
 
 export default function GoogleCallbackPage() {
-  const router = useRouter();
-  const params = useSearchParams();
-  const token = params.get("access_token");
-
-  useEffect(() => {
-    if (token) {
-      localStorage.setItem("accessToken", token);
-      router.push("/cloud/recents");
-    }
-  }, [token, router]);
-
   return (
-    <div className="font-bold h-screen w-full flex flex-col  justify-center gap-3 items-center p-4">
-      <div className="size-16">
-        <LoadingSpinner />
-      </div>
-      <p>Logging you in with Google...</p>
-    </div>
+    <Suspense fallback={<LoadingSpinner />}>
+      <GoogleCallback />
+    </Suspense>
   );
 }
